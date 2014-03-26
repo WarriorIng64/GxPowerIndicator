b622e98fe29c4766b50ffb4e417a009e5f017b4e
Christopher Kyle Horton <christhehorton@gmail.com>
2014-03-26 16:26:00 -0400

    Rename indicator and source.

--- GxPowerIndicator.py
+++ GxPowerIndicator.py
@@ -1,21 +1,21 @@
-# This file is part of PowerIndicator.
+# This file is part of GxPowerIndicator.
 # Copyright (C) 2014 Christopher Kyle Horton <christhehorton@gmail.com>
 
-# PowerIndicator is free software: you can redistribute it and/or modify
+# GxPowerIndicator is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# PowerIndicator is distributed in the hope that it will be useful,
+# GxPowerIndicator is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with PowerIndicator. If not, see <http://www.gnu.org/licenses/>.
+# along with GxPowerIndicator. If not, see <http://www.gnu.org/licenses/>.
 
 
-# PowerIndicator
+# GxPowerIndicator
 # This indicator shuts down the SubOS when clicked.
 
 self.SetIcon(pygame.image.load("indicators/default/GxPowerIndicator/shutdown.png")
